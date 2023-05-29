471fa61a48b59f067a3396c9c726696beb6df67c
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-29 17:07:58 +0200

    modified

--- myscript.js
+++ myscript.js
@@ -1,46 +1,41 @@
 
 
 const teamMembers = {
-    {nome: 'Waune'
+    {
+        nome: 'Waune'
         surname: 'Barnett'
         role: 'Founder & CEO'
-        'profile picture': 'wayne-barnett-founder-ceo.jpg'
     }
 
     {
         nome: 'Angela'
         surname: 'Caroll'
         role: 'Chef Editor'
-        'profile picture': 'angela-caroll-chief-editor.jpg'
     }
 
     {
         nome: 'Walter'
         surname: 'Gordon'
         role: 'Office Manager'
-        'profile picture': ' walter-gordon-office-manager.jpg'
     }
 
     {
         nome:'Angela'
         surname: 'Lopez'
         role: 'Social Media'
-        "profile picture" : 'èangela-lopez-social-media-manager.jpg'
     }
 
     {
         nome: 'Scott'
         surname:'Estrada'
         role:'Developer'
-        'profile picture': 'scott-estrada-developer.jpg'
     }
 
     {
         nome:'Barbara'
         surname:'Ramos'
         role:'Graphic Designer'
-        'profile picture': 'barbara-ramos-graphic-designer.jpg'
     }
-};
+}
 
 console.log(teamMembers);
