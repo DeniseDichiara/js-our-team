ead7fa4e86c1cf8d393a893bf56389d65cdf6a8d
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-29 17:20:00 +0200

    added profilePicture

--- myscript.js
+++ myscript.js
@@ -6,36 +6,42 @@ const teamMembers = [
         nome: 'Waune',
         surname: 'Barnett',
         role: 'Founder & CEO',
+        profilePicture: 'wayne-barnett-founder-ceo.jpg'
     },
 
     {
         nome: 'Angela',
         surname: 'Caroll',
         role: 'Chef Editor',
+        profilePicture: 'angela-caroll-chief-editor.jpg'
     },
 
     {
         nome: 'Walter',
         surname: 'Gordon',
         role: 'Office Manager',
+        profilePicture: 'walter-gordon-office-manager.jpg'
     },
 
     {
         nome:'Angela',
         surname: 'Lopez',
         role: 'Social Media',
+        profilePicture: 'angela-lopez-social-media-manager.jpg'
     },
 
     {
         nome: 'Scott',
         surname:'Estrada',
         role:'Developer',
+        profilePicture: 'scott-estrada-developer.jpg'
     },
 
     {
         nome:'Barbara',
         surname:'Ramos',
-        role:'Graphic Designer'
+        role:'Graphic Designer',
+        profilePicture: 'barbara-ramos-graphic-designer.jpg'
     }
 ]
     
