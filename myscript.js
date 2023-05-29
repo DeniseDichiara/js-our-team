e8afa2ea9418d137551ce3229c5c1c1874a411ee
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-29 16:39:56 +0200

    array

---
+++ myscript.js
@@ -0,0 +1,39 @@
+
+
+const teamMembers = {
+    {
+        nome: 'Waune'
+        surname: 'Barnett'
+        role: 'Founder & CEO'
+    }
+
+    {
+        nome: 'Angela'
+        surname: 'Caroll'
+        role: 'Chef Editor'
+    }
+
+    {
+        nome: 'Walter'
+        surname: 'Gordon'
+        role: 'Office Manager'
+    }
+
+    {
+        nome:'Angela'
+        surname: 'Lopez'
+        role: 'Social Media'
+    }
+
+    {
+        nome: 'Scott'
+        surname:'Estrada'
+        role:'Developer'
+    }
+
+    {
+        nome:'Barbara'
+        surname:'Ramos'
+        role:'Graphic Designer'
+    }
+};
